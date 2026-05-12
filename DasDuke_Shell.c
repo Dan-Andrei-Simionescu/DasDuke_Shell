2d5b9928a36c02f75a5f3072a001d18fe8deff96
Dan-Andrei-Simionescu <andreidas04@gmail.com>
2026-05-12 11:50:17 +0300

    Fix: redundant appellation of execvp() function.

--- DasDuke_Shell.c
+++ DasDuke_Shell.c
@@ -5,14 +5,13 @@
 #include <sys/wait.h>
 
 void execution_of_command(char* args[64]) {
-    size_t random_pid = fork();
+    ssize_t random_pid = fork();
 
     if (random_pid == -1) {
         perror("Something went wrong when creating the child process.");
         exit(EXIT_FAILURE);
     } else if (random_pid == 0){
         // Child's Timeline (sees itself as 0)
-        execvp(args[0], args);
         if (execvp(args[0], args) == -1) {
             perror("Invalid Command, stop all operations");
             exit(EXIT_FAILURE);
